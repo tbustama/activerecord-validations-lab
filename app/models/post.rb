7d457068c1cc57009df5cd467bbacabe839767bd
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-17 16:33:42 -0800

    commit tree ebb51a9ef54095ad11d63e081fe7995bc2bb52c0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :non_click_bait
+
+    def non_click_bait
+        if title
+            bait = ["Won't Believe", "Secret", "Top", "Guess"].any? {|phrase| title.include? (phrase)}
+            bait == false && errors.add(:title, "needs more clickbait")
+        end
+    end
 end
